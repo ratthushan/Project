11fd699f9bcb8ff2a364d9aacfb9084a749e16a3
Ratthushan Jeevanadan <p15202558@my365.dmu.ac.uk>
2017-12-17 20:34:34 +0000

    Gallery
    Added a gallery feature using javascript

--- main.js
+++ main.js
@@ -1,4 +1,38 @@
 
+var elements = document.getElementsByClassName("column");
+
+// Declare a loop variable
+var c;
+
+// Four images side by side
+function four() {
+    for (c = 0; c < elements.length; c++) {
+        elements[c].style.width = "25%";
+    }
+}
+
+// Two images side by side
+function two() {
+    for (c = 0; c < elements.length; c++) {
+        elements[c].style.width = "50%";
+    }
+}
+
+
+
+// Add active class to the current button (highlight it)
+var header = document.getElementById("myHeader");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; c < btns.length; c++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+
+
 function openNav() {
     document.getElementById("mySidenav").style.width = "200px";
     document.getElementById("main").style.marginLeft = "250px";
@@ -7,19 +41,4 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
-}
-
-var myIndex = 0;
-carousel();
-
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";  
-    }
-    myIndex++;
-    if (myIndex > x.length) {myIndex = 1}    
-    x[myIndex-1].style.display = "block";  
-    setTimeout(carousel, 9000);    
 }
